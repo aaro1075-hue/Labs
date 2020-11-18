58db6c7e9b7278b56811c4abbec440fd69c691f4
aaro1075-hue <emily.l.aaron-1@ou.edu>
2020-11-17 21:19:08 -0600

    initial

---
+++ myboot 2.R
@@ -0,0 +1,25 @@
+#'myboot2
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+myboot<-function(iter=10000,x,fun="mean",alpha=0.05,...){
+n=length(x)
+y=sample(x,n*iter,replace=TRUE)
+rs.mat=matrix(y,nr=n,nc=iter,byrow=TRUE)
+xstat=apply(rs.mat,2,fun)
+ci=quantile(xstat,c(alpha/2,1-alpha/2))
+para=hist(xstat,freq=FALSE,las=1,main="Histogram of Bootstrap sample statistics",...)
+mat=matrix(x,nr=length(x),nc=1,byrow=TRUE)
+pte=apply(mat,2,fun)
+abline(v=pte,lwd=3,col="Black")
+segments(ci[1],0,ci[2],0,lwd=4)
+text(ci[1],0,paste("(",round(ci[1],2),sep=""),col="Red",cex=3)
+text(ci[2],0,paste(round(ci[2],2),")",sep=""),col="Red",cex=3)
+text(pte,max(para$density)/2,round(pte,2),cex=3)
+return(list(ci=ci,fun=fun,x=x))
+}
